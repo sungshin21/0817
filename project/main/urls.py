399e92ecbbebede1287925c652319645238d1208
handagyeong <80438961+handagyeong@users.noreply.github.com>
2021-08-17 17:34:01 +0900

    gg
    gg

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
+
 from . import views
 from main import views
-
+from main.views import HomeView
+from main.views import UserCreateView, UserCreateDoneTV
 
 
 urlpatterns = [
@@ -9,7 +13,12 @@ urlpatterns = [
     path('/seoul', views.seoul ),
     # path('/경기', views.gg ),
     # path('main/', include('main_patterns')),
- 
+     path('admin/', admin.site.urls),
+
+    path('', HomeView.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
     # path('post/<int:pk>/', views.local_detail, name='local_detail'),
 
 ]
